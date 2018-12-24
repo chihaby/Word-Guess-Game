14dea09fc8b6766d8c7be63d45c84e9feef79cfa
Rad <Rad@Rad.local>
2018-12-24 13:37:32 -0800

    code update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,21 @@
-// declaring counters
+// alert('press any OK to start');
+// declaring counters and variables
 var wins = '0';
 var remaining = 0;
 var guessed = 0;
-var singersArr = ['madonna', 'prince', 'michael'];
 
-function singers(){
-    singersArr.forEach(function(singer){
-        console.log(singer.split(''));
-    })
-}
 
-singers(1);
-// var hidden0 = singers.replace(/^[a-zA-Z]+$/g , '-');
+var singerA = 'queen';
+var singerB = 'clapton';
+var singerC = 'hendrix';
 
-// alert('press any OK to start');
+var splA = singerA.split('');
+var splB = singerB.split('');
+var splC = singerC.split('');
+
+var hiddenA = singerA.replace(/[a-zA-Z]/g, '-');
+var hiddenB = singerB.replace(/[a-zA-Z]/g, '-');
+var hiddenC = singerC.replace(/[a-zA-Z]/g, '-');
 
 // declaring object literal titled "game" that will hold all propreties, values and methods. 
 var game = {
@@ -23,13 +25,40 @@ var game = {
 // function userInput checks user input for corresponding letters
 function userInput() {
     document.addEventListener('keydown', function(){
-        if (event.key.toLowerCase() === 'q'){
+        // for (var i=0; splA.length; i++){
+          if (event.key.toLowerCase() === 'q'){
             wins++; remaining++; guessed++;
             console.log('test passed');
             console.log(`Wins: ${wins} Remaining: ${remaining} Guessed: ${guessed}`);
-        }
+          }  
+        // }
     });
 }
+userInput();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(splA[0]);
+// console.log(splB);
+// console.log(splC);
+// console.log(hiddenA);
+// console.log(hiddenB);
+// console.log(hiddenC);
 
 
