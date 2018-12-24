dda1a8ea468a632f259cffc0e8bb500ad561b09a
Rad <rad@Rads-MacBook-Pro.local>
2018-12-24 12:01:12 -0800

    updates js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,19 @@
 // declaring counters
-var wins = 0;
+var wins = '0';
 var remaining = 0;
-var gussed = 0;
+var guessed = 0;
+var singersArr = ['madonna', 'prince', 'michael'];
+
+function singers(){
+    singersArr.forEach(function(singer){
+        console.log(singer.split(''));
+    })
+}
+
+singers(1);
+// var hidden0 = singers.replace(/^[a-zA-Z]+$/g , '-');
+
+// alert('press any OK to start');
 
 // declaring object literal titled "game" that will hold all propreties, values and methods. 
 var game = {
@@ -11,10 +23,13 @@ var game = {
 // function userInput checks user input for corresponding letters
 function userInput() {
     document.addEventListener('keydown', function(){
-        if (event.key === 'q'){
-            console.log('test passed')
+        if (event.key.toLowerCase() === 'q'){
+            wins++; remaining++; guessed++;
+            console.log('test passed');
+            console.log(`Wins: ${wins} Remaining: ${remaining} Guessed: ${guessed}`);
         }
     });
 }
 
-game.key;
+
+
