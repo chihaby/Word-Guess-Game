7369b62fc19ccf3792d0a3f483fb97de1fddab29
Rad <rad@Rads-MacBook-Pro.local>
2018-12-24 15:25:11 -0800

    more code updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,61 +4,36 @@ var wins = '0';
 var remaining = 0;
 var guessed = 0;
 
-
-var singerA = 'queen';
-var singerB = 'clapton';
-var singerC = 'hendrix';
-
-var splA = singerA.split('');
-var splB = singerB.split('');
-var splC = singerC.split('');
-
-var hiddenA = singerA.replace(/[a-zA-Z]/g, '-');
-var hiddenB = singerB.replace(/[a-zA-Z]/g, '-');
-var hiddenC = singerC.replace(/[a-zA-Z]/g, '-');
-
-// declaring object literal titled "game" that will hold all propreties, values and methods. 
 var game = {
-    key : userInput()
+  singerA : 'queen',
+  singerB : 'clapton',
+  singerC : 'hendrix',
+  key : userInput()
 }
 
-// function userInput checks user input for corresponding letters
+spltA = game.singerA.split('');
+spltB = game.singerB.split('');
+spltC = game.singerC.split('');
+
+hiddenA = game.singerA.replace(/[a-zA-Z]/g, '-');
+hiddenB = game.singerB.replace(/[a-zA-Z]/g, '-');
+hiddenC = game.singerC.replace(/[a-zA-Z]/g, '-');
+
 function userInput() {
     document.addEventListener('keydown', function(){
-        // for (var i=0; splA.length; i++){
-          if (event.key.toLowerCase() === 'q'){
+        spltA.forEach(letter => {
+           if (event.key.toLowerCase() === letter){
             wins++; remaining++; guessed++;
-            console.log('test passed');
-            console.log(`Wins: ${wins} Remaining: ${remaining} Guessed: ${guessed}`);
+            console.log(`TEST PASSED ===> Wins: ${wins} Remaining: ${remaining} Guessed: ${guessed}`);
           }  
-        // }
+        })
     });
 }
-userInput();
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(splA[0]);
-// console.log(splB);
-// console.log(splC);
-// console.log(hiddenA);
-// console.log(hiddenB);
-// console.log(hiddenC);
 
 
