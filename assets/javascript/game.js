678fc98142b60cd59eda8b1d9ec775140584aa4e
Radouane <38984550+Radouane@users.noreply.github.com>
2018-12-26 13:25:14 -0800

    sticking with original plan

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,40 @@
-// alert('press any OK to start');
-// declaring counters and variables
-var wins = '0';
-var remaining = 0;
-var guessed = 0;
+
 
 var game = {
   singerA : 'queen',
   singerB : 'clapton',
   singerC : 'hendrix',
-  key : userInput()
+  wins : 0,
+  remaining : 4,
+  correctGuess: '',
+  wrongGuess : '',
+  goodSound: new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/goodbell.mp3"),
+  badSound: new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/bad.mp3"),
+  winSound: new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/win.mp3"),
+  loseSound: new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/lose.mp3"),
+  init : main()
 }
 
-spltA = game.singerA.split('');
-spltB = game.singerB.split('');
-spltC = game.singerC.split('');
+var splitA = game.singerA.split('');
+var splitB = game.singerB.split('');
+var splitC = game.singerC.split('');
 
-hiddenA = game.singerA.replace(/[a-zA-Z]/g, '-');
-hiddenB = game.singerB.replace(/[a-zA-Z]/g, '-');
-hiddenC = game.singerC.replace(/[a-zA-Z]/g, '-');
+var userInput = $(".guessLetter").value;
 
-function userInput() {
+function main() {
     document.addEventListener('keydown', function(){
-        spltA.forEach(letter => {
-           if (event.key.toLowerCase() === letter){
-            wins++; remaining++; guessed++;
-            console.log(`TEST PASSED ===> Wins: ${wins} Remaining: ${remaining} Guessed: ${guessed}`);
+          if (splitA.includes(userInput)){
+            game.wins; game.remaining; game.correctGuess += event.key; game.goodSound.play();
+           console.log (`TEST PASSED ===> Wins: ${game.wins}   Remaining: ${game.remaining}   Correct Guesses: ${game.correctGuess}`);
           }  
-        })
+          if (!splitA.includes(event.key.toLowerCase())){
+            game.wins; game.remaining--; game.wrongGuess += event.key; game.badSound.play();
+           console.log (`TEST NOT PASSED ===> Wins: ${game.wins}   Remaining: ${game.remaining}   Guessed Wrong: ${game.wrongGuess} `);
+          } 
+          if (game.remaining === 0){
+            game.loseSound.play();
+            console.log(' Game Over - reload page and try again')
+          }        
     });
 }
 
@@ -37,3 +45,12 @@ function userInput() {
 
 
 
+
+
+
+
+
+
+// hiddenA = game.singerA.replace(/[a-zA-Z]/g, '-');
+// hiddenB = game.singerB.replace(/[a-zA-Z]/g, '-');
+// hiddenC = game.singerC.replace(/[a-zA-Z]/g, '-');
