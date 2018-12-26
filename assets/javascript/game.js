a5efed8b932b50334f74aa640cd3bc6d449e1920
Radouane <38984550+Radouane@users.noreply.github.com>
2018-12-26 14:42:25 -0800

    more updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ var game = {
   badSound: new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/bad.mp3"),
   winSound: new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/win.mp3"),
   loseSound: new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/lose.mp3"),
-  init : main()
+  // init : main()
 }
 
 var splitA = game.singerA.split('');
@@ -22,25 +22,21 @@ var splitC = game.singerC.split('');
 var x = document.getElementById("userInput");
 
 function main() {
-    document.addEventListener('keydown', function(){
           if (splitA.includes(x.value)){
-            console.log(x.value);
             game.wins; game.remaining; game.correctGuess += x.value; game.goodSound.play();
-           console.log (`TEST PASSED ===> Wins: ${game.wins}   Remaining: ${game.remaining}   Correct Guesses: ${game.correctGuess}`);
+           console.log (`PASSED ===> Wins: ${game.wins}   Remaining: ${game.remaining}   Correct Guesses: ${game.correctGuess}`);
           }  
           if (!splitA.includes(x.value)){
-            console.log(x.value);
             game.wins; game.remaining--; game.wrongGuess += x.value; game.badSound.play();
-           console.log (`TEST NOT PASSED ===> Wins: ${game.wins}   Remaining: ${game.remaining}   Guessed Wrong: ${game.wrongGuess} `);
+           console.log (`- NOT - PASSED ===> Wins: ${game.wins}   Remaining: ${game.remaining}   Guessed Wrong: ${game.wrongGuess} `);
           } 
           if (game.remaining === 0){
             game.loseSound.play();
             console.log(' Game Over - reload page and try again')
           }  
           else {
-            document.getElementById("demo").innerHTML = "The correct word is: " + "insert word";
+            // document.getElementById("demo").innerHTML = "The correct word is: " + "insert word";
           }      
-    });
 }
 
 
